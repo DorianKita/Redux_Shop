4116720a89b3a694e0fa2db28de6917a940f79cd
Dorian Kita <dorian.kita90@gmail.com>
2024-06-12 21:01:26 +0200

    connected reducer to button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
 
 function App() {
+  const state = useSelector((state) => state.ui.cartIsVisible);
+
   return (
     <Layout>
-      <Cart />
+      {state && <Cart />}
       <Products />
     </Layout>
   );
