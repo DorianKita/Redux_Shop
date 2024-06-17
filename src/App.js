7927ef670971a11421a54cc35ca18ef69906166d
Dorian Kita <dorian.kita90@gmail.com>
2024-06-17 18:06:10 +0200

    created and dispatched notification in App component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const dispatch = useDispatch();
@@ -11,6 +12,13 @@ function App() {
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data...",
+        })
+      );
       const response = await fetch(
         "https://react-learn-t-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         { method: "PUT", body: JSON.stringify(cart) }
@@ -19,8 +27,24 @@ function App() {
       if (!response.ok) {
         throw new Error("Sending data did not passed");
       }
-      const responseData = await response.json();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Send cart data ",
+        })
+      );
     };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error...",
+          message: "Sending cart data failed...",
+        })
+      );
+    });
   }, [cart]);
 
   return (
