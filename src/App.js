dc78d7b4726ae079ad3ffbacd50f6018efdbdf52
Dorian Kita <dorian.kita90@gmail.com>
2024-06-17 17:22:43 +0200

    created new reducer for Notification component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch(
-      "https://react-learn-t-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      { method: "PUT", body: JSON.stringify(cart) }
-    );
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://react-learn-t-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending data did not passed");
+      }
+      const responseData = await response.json();
+    };
   }, [cart]);
 
   return (
