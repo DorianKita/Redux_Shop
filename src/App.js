5a6ff066bc73192e9dd468ada3761256e6919fda
Dorian Kita <dorian.kita90@gmail.com>
2024-06-17 18:18:58 +0200

    created error handling and developed way to start app without sending initial request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const state = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -36,6 +40,11 @@ function App() {
       );
     };
 
+    if (isInitial) {
+      isInitial = !isInitial;
+      return;
+    }
+
     sendCartData().catch((error) => {
       dispatch(
         uiActions.showNotification({
@@ -45,13 +54,22 @@ function App() {
         })
       );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {state && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {state && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
