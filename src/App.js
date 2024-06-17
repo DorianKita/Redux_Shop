0ab09bbfdd9c8be6c31060da6f22c3020ab06673
Dorian Kita <dorian.kita90@gmail.com>
2024-06-17 16:59:45 +0200

    sync cart with google firebase to store cart items

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const state = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-learn-t-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
